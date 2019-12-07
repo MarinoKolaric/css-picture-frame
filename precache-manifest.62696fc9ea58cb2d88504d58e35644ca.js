bdd848e47f062de44e262e6dfb96e56bbe659986
MarinoKolaric <marinokolaric@gmail.com>
2019-12-07 11:45:48 +0100

    Updates

---
+++ precache-manifest.62696fc9ea58cb2d88504d58e35644ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd271dd96bc72b1aa32657d157309258",
+    "url": "/css-picture-frame/index.html"
+  },
+  {
+    "revision": "9e57ecc82e8378f54eba",
+    "url": "/css-picture-frame/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "36537c9fb59ea868bc5a",
+    "url": "/css-picture-frame/static/js/2.8ae36307.chunk.js"
+  },
+  {
+    "revision": "9e57ecc82e8378f54eba",
+    "url": "/css-picture-frame/static/js/main.347857e9.chunk.js"
+  },
+  {
+    "revision": "1d4e86b625b56f85b0c0",
+    "url": "/css-picture-frame/static/js/runtime-main.0c26a0ad.js"
+  },
+  {
+    "revision": "6b02d831e1f590315d969fba043ca594",
+    "url": "/css-picture-frame/static/media/petra.6b02d831.jpeg"
+  }
+]);
